dc94e5aa74d88852c1b83e9424ef28f1532d235b
Grace <ygleam1206@qq.com>
2026-07-09 22:46:24 +0800

    fix: support render deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { getDb } = require('./backend/database/connection');
 getDb();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 中间件
 app.use(express.json());
